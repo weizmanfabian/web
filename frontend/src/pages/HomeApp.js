5674d4ad1ca7884a41ffd1e77cfbe4ecf3bab170
weizmanfabian <weizmanfabian@gmail.com>
2022-05-30 21:25:51 -0500

    upload image with select. Navbar links

--- frontend/src/pages/HomeApp.js
+++ frontend/src/pages/HomeApp.js
@@ -1,6 +1,21 @@
+import React from 'react';
+
+import img from '../assets/img/hola.jpg'
+
 const HomeApp = ({ user }) => {
+
     return (
-        <h1>Bienvenido {user.nombre} es un gusto tenerte con nosotros</h1>
+        <div className='container'>
+            <div className="row text-center align-items-center justify-content-center">
+                <div className="card" style={{ "width": "30rem" }}>
+                    <img src={img} className="card-img-top" alt="Imagen" />
+                    <div className="card-body">
+                        <h1>Bienvenido {user.nombre} es un gusto tenerte con nosotros</h1>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     );
 }
 
--- frontend/src/routes/AplicationRoute.js
+++ frontend/src/routes/AplicationRoute.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import HomeApp from "../pages/HomeApp";
 import Navbar from "../components/Navbar.js"
+import UploadImage from "../pages/UploadImage";
 
 const AplicationRoute = ({ user, setUser }) => {
     return (
@@ -8,6 +9,7 @@ const AplicationRoute = ({ user, setUser }) => {
             <Navbar user={user} setUser={setUser} />
             <Routes>
                 <Route path="/home" element={<HomeApp user={user} />} />
+                <Route path="/uploadImage" element={<UploadImage user={user} />} />
             </Routes>
         </>
     );
